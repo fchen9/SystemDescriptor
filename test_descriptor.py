4bd0fb2a9c9a664900e9c615fec856555e63f86e
vancristi <dcsterpu@assystem.com>
2018-04-13 16:05:52 +0300

    Test plan

--- test_descriptor.py
+++ test_descriptor.py
@@ -169,7 +169,7 @@ class SystemDescriptor(unittest.TestCase):
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\\TRS.SYSDESC.FUNC.002_01\\ConfigSystemDescriptor.xml')
     #     self.assertTrue(FileCompare.areSame(head+'\\Tests\\TRS.SYSDESC.FUNC.002_01\\SystemGenerated.arxml', head+'\\Tests\\TRS.SYSDESC.FUNC.002_01\\Output\\SystemGenerated.arxml'))
-    # #
+
     # def test_TRS_SYSDESC_FUNC_002_02(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
@@ -182,28 +182,38 @@ class SystemDescriptor(unittest.TestCase):
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\\TRS.SYSDESC.FUNC.002_03\\ConfigSystemDescriptor.xml')
     #     self.assertTrue(FileCompare.areSame(head+'\\Tests\\TRS.SYSDESC.FUNC.002_03\\SystemGenerated.arxml', head+'\\Tests\\TRS.SYSDESC.FUNC.002_03\\Output\\SystemGenerated.arxml'))
-    #
+
     # def test_TRS_SYSDESC_FUNC_002_04(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\\TRS.SYSDESC.FUNC.002_04\\ConfigSystemDescriptor.xml')
     #     self.assertFalse(FileCompare.isOutput(head+'\\Tests\\TRS.SYSDESC.FUNC.002_04\\Output\\SystemGenerated.arxml'))
     #     self.assertTrue(FileCompare.checkLog(head + '\\Tests\\TRS.SYSDESC.FUNC.002_04\\output\\result.log', "ERROR", ["The interface: tBinaire"]))
-
-    # def test_TRS_SYSDESC_FUNC_002_05(self):   ##############
+    #
+    # def test_TRS_SYSDESC_FUNC_002_05(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\\TRS.SYSDESC.FUNC.002_05\\ConfigSystemDescriptor.xml')
     #     self.assertTrue(FileCompare.areSame(head+'\\Tests\\TRS.SYSDESC.FUNC.002_05\\SystemGenerated.arxml', head+'\\Tests\\TRS.SYSDESC.FUNC.002_05\\Output\\SystemGenerated.arxml'))
-
-    # def test_TRS_SYSDESC_FUNC_002_07(self):   ##############
+    #
+    # def test_TRS_SYSDESC_FUNC_002_06(self):
+    #     current_path = os.path.realpath(__file__)
+    #     head, tail = ntpath.split(current_path)
+    #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\\TRS.SYSDESC.FUNC.002_06\\ConfigSystemDescriptor.xml')
+    #     self.assertTrue(FileCompare.areSame(head+'\\Tests\\TRS.SYSDESC.FUNC.002_06\\SystemGenerated.arxml', head+'\\Tests\\TRS.SYSDESC.FUNC.002_06\\Output\\SystemGenerated.arxml'))
+    #
+    # def test_TRS_SYSDESC_FUNC_002_07(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\\TRS.SYSDESC.FUNC.002_07\\ConfigSystemDescriptor.xml')
     #     self.assertTrue(FileCompare.areSame(head+'\\Tests\\TRS.SYSDESC.FUNC.002_07\\SystemGenerated.arxml', head+'\\Tests\\TRS.SYSDESC.FUNC.002_07\\Output\\SystemGenerated.arxml'))
-
-
-
+    #
+    # def test_TRS_SYSDESC_FUNC_002_08(self):
+    #     current_path = os.path.realpath(__file__)
+    #     head, tail = ntpath.split(current_path)
+    #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\\TRS.SYSDESC.FUNC.002_08\\ConfigSystemDescriptor.xml')
+    #     self.assertTrue(FileCompare.areSame(head+'\\Tests\\TRS.SYSDESC.FUNC.002_08\\SystemGenerated.arxml', head+'\\Tests\\TRS.SYSDESC.FUNC.002_08\\Output\\SystemGenerated.arxml'))
+    #
     # def test_TRS_SYSDESC_GEN_002_01(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
@@ -256,7 +266,6 @@ class SystemDescriptor(unittest.TestCase):
     #     self.assertTrue(FileCompare.areSame(head + '\\Tests\\TRS.SYSDESC.GEN.003_4\\SystemGenerated.arxml',
     #                                         head + '\\Tests\\TRS.SYSDESC.GEN.003_4\\Output\\SystemGenerated.arxml'))
     #
-    #
     # def test_TRS_SYSDESC_GEN_003_05(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
@@ -291,35 +300,42 @@ class SystemDescriptor(unittest.TestCase):
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.GEN.003_9\\ConfigSystemDescriptor.xml')
     #     self.assertTrue(FileCompare.areSame(head + '\\Tests\\TRS.SYSDESC.GEN.003_9\\SystemGenerated.arxml',
     #                                         head + '\\Tests\\TRS.SYSDESC.GEN.003_9\\Output\\SystemGenerated.arxml'))
-
+    #
     # def test_TRS_SYSDESC_GEN_003_10(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.GEN.003_10\\ConfigSystemDescriptor.xml')
     #     self.assertFalse(FileCompare.isOutput(head+'\\Tests\\TRS.SYSDESC.GEN.003_10\\Output\\SystemGenerated.arxml'))
     #     self.assertTrue(FileCompare.checkLog(head + '\\Tests\\TRS.SYSDESC.GEN.003_10\\output\\result.log', "ERROR", ["SW-COMPONENT-PROTOTYPE"]))
-
+    #
     # def test_TRS_SYSDESC_GEN_003_11(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.GEN.003_11\\ConfigSystemDescriptor.xml')
     #     self.assertTrue(FileCompare.areSame(head + '\\Tests\\TRS.SYSDESC.GEN.003_11\\SystemGenerated.arxml',
     #                                         head + '\\Tests\\TRS.SYSDESC.GEN.003_11\\Output\\SystemGenerated.arxml'))
-
+    #
+    # def test_TRS_SYSDESC_GEN_003_12(self):
+    #     current_path = os.path.realpath(__file__)
+    #     head, tail = ntpath.split(current_path)
+    #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.GEN.003_12\\ConfigSystemDescriptor.xml')
+    #     self.assertTrue(FileCompare.areSame(head + '\\Tests\\TRS.SYSDESC.GEN.003_12\\SystemGenerated.arxml',
+    #                                         head + '\\Tests\\TRS.SYSDESC.GEN.003_12\\Output\\SystemGenerated.arxml'))
+    #
     # def test_TRS_SYSDESC_GEN_003_13(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.GEN.003_13\\ConfigSystemDescriptor.xml')
     #     self.assertTrue(FileCompare.areSame(head + '\\Tests\\TRS.SYSDESC.GEN.003_13\\SystemGenerated.arxml',
     #                                         head + '\\Tests\\TRS.SYSDESC.GEN.003_13\\Output\\SystemGenerated.arxml'))
-
+    #
     # def test_TRS_SYSDESC_GEN_003_14(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.GEN.003_14\\ConfigSystemDescriptor.xml')
     #     self.assertFalse(FileCompare.isOutput(head+'\\Tests\\TRS.SYSDESC.GEN.003_14\\Output\\SystemGenerated.arxml'))
     #     self.assertTrue(FileCompare.checkLog(head + '\\Tests\\TRS.SYSDESC.GEN.003_14\\output\\result.log', "ERROR", ["ROOT-SW-COMPOSITION-PROTOTYPE"]))
-
+    #
     # def test_TRS_SYSDESC_GEN_003_15(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
@@ -333,7 +349,7 @@ class SystemDescriptor(unittest.TestCase):
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.GEN.003_16\\ConfigSystemDescriptor.xml')
     #     self.assertFalse(FileCompare.isOutput(head+'\\Tests\\TRS.SYSDESC.GEN.003_16\\Output\\SystemGenerated.arxml'))
     #     self.assertTrue(FileCompare.checkLog(head + '\\Tests\\TRS.SYSDESC.GEN.003_16\\output\\result.log', "ERROR", ["Multiple <SYSTEM-VERSION>"]))
-
+    #
     # def test_TRS_SYSDESC_GEN_004_1(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
@@ -359,27 +375,27 @@ class SystemDescriptor(unittest.TestCase):
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.FUNC.200_2\\ConfigSystemDescriptor.xml')
     #     self.assertTrue(FileCompare.checkLog(head + '\\Tests\\TRS.SYSDESC.FUNC.200_2\\output\\result.log', "INFO", ["SENDER-RECEIVER-TO-SIGNAL-MAPPING using the signal/RootP_NetworkDesc/AUTONOMIE_HS1_52E"]))
-
+    #
     # def test_TRS_SYSDESC_1(self):
     #     current_path = os.path.realpath(__file__)
     #     head, tail = ntpath.split(current_path)
     #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.1\\ConfigSystemDescriptor.xml')
     #     self.assertFalse(FileCompare.isOutput(head+'\\Tests\\TRS.SYSDESC.1\\Output\\SystemGenerated.arxml'))
     #     self.assertTrue(FileCompare.checkLog(head + '\\Tests\\TRS.SYSDESC.1\\output\\result.log', "ERROR", ["ASWC_A26.aswc.arxml"]))
+    #
+    def test_TRS_SYSDESC_2(self):
+        current_path = os.path.realpath(__file__)
+        head, tail = ntpath.split(current_path)
+        os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.2\\ConfigSystemDescriptor.xml')
+        self.assertFalse(FileCompare.isOutput(head+'\\Tests\\TRS.SYSDESC.2\\Output\\SystemGenerated.arxml'))
+        self.assertTrue(FileCompare.checkLog(head + '\\Tests\\TRS.SYSDESC.2\\output\\result.log', "ERROR", ["ASWC_A26.mappings1.dico"]))
+    #
 
-    # def test_TRS_SYSDESC_2(self):
-    #     current_path = os.path.realpath(__file__)
-    #     head, tail = ntpath.split(current_path)
-    #     os.system('SystemDescriptor.py -config ' + head + '\\Tests\TRS.SYSDESC.2\\ConfigSystemDescriptor.xml')
-    #     self.assertFalse(FileCompare.isOutput(head+'\\Tests\\TRS.SYSDESC.2\\Output\\SystemGenerated.arxml'))
-    #     self.assertTrue(FileCompare.checkLog(head + '\\Tests\\TRS.SYSDESC.2\\output\\result.log', "ERROR", ["ASWC_A26.mappings1.dico"]))
 
+# suite = unittest.TestLoader().loadTestsFromTestCase(SystemDescriptor)
+# unittest.TextTestRunner(verbosity=2).run(suite)
 
-
-suite = unittest.TestLoader().loadTestsFromTestCase(SystemDescriptor)
-unittest.TextTestRunner(verbosity=2).run(suite)
-
-# current_path = os.path.realpath(__file__)
-# head, tail = ntpath.split(current_path)
-# if __name__ == "__main__":
-#     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output=head + "\\tests"))
+current_path = os.path.realpath(__file__)
+head, tail = ntpath.split(current_path)
+if __name__ == "__main__":
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output=head + "\\tests"))
